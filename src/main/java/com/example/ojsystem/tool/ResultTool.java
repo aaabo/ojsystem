1f0e547c6d806db2b5ed94c580e8c1195b428ad6
aaabo <767324214@qq.com>
2021-09-06 16:57:33 +0800

    前台图表的后台代码

---
+++ src/main/java/com/example/ojsystem/tool/ResultTool.java
@@ -0,0 +1,22 @@
+package com.example.ojsystem.tool;
+
+public class ResultTool {
+    private String exerciseResult;
+    private int number;
+
+    public String getExerciseResult() {
+        return exerciseResult;
+    }
+
+    public void setExerciseResult(String exerciseResult) {
+        this.exerciseResult = exerciseResult;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
